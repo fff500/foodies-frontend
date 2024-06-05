87cc72f6d870e15baeb20951c89b00458d47dac4
Pavlo Tereshko <pavlo.tereshko@gmail.com>
2024-06-05 13:44:16 +0300

    Prettier was added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import { lazy } from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { lazy } from "react";
+import { Routes, Route } from "react-router-dom";
 
-import SharedLayout from './components/layout/SharedLayout/SharedLayout';
+import SharedLayout from "./components/layout/SharedLayout/SharedLayout";
 
-const Home = lazy(() => import('./pages/Home/Home'));
-const User = lazy(() => import('./pages/User/User'));
-const Recipe = lazy(() => import('./pages/Recipe/Recipe'));
+const Home = lazy(() => import("./pages/Home/Home"));
+const User = lazy(() => import("./pages/User/User"));
+const Recipe = lazy(() => import("./pages/Recipe/Recipe"));
 
 const App = () => (
   <Routes>
--- src/components/layout/SharedLayout/SharedLayout.jsx
+++ src/components/layout/SharedLayout/SharedLayout.jsx
@@ -1,7 +1,7 @@
-import { Suspense } from 'react';
-import { Outlet } from 'react-router-dom';
+import { Suspense } from "react";
+import { Outlet } from "react-router-dom";
 
-import { Header } from '../Header/Header';
+import { Header } from "../Header/Header";
 
 const SharedLayout = () => {
   return (
