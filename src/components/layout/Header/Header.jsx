748dc4fa793b736e0b1108660d1cdbdbcdf9b0d0
VorobeiIvan <0675361905vi@gmail.com>
2024-06-06 23:54:58 +0300

    fix

--- src/components/layout/Header/Header.jsx
+++ src/components/layout/Header/Header.jsx
@@ -3,7 +3,7 @@ import HeaderNav from "../../HeaderNav";
 import User from "../../../pages/User";
 import Logo from "../../Logo";
 import styles from "./Header.module.css";
-import AuthButtons from "../../AuthButtons";
+// import AuthButtons from "../../AuthButtons";
 
 const Header = () => {
   return (
@@ -11,9 +11,9 @@ const Header = () => {
       <Link to="/">
         <Logo />
       </Link>
-      <AuthButtons />
-      {/* <HeaderNav />
-      <User userImage={"data.serImage"} userName={"data.userName"} /> */}
+      {/* <AuthButtons /> */}
+      <HeaderNav />
+      <User userImage={"data.serImage"} userName={"data.userName"} />
     </header>
   );
 };
