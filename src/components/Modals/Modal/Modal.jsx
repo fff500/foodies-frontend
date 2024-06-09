2f185e77bb43c3d5a6a14d3efc009a72c2269482
Slava Poliakov <78slavapolyakov@gmail.com>
2024-06-09 15:05:08 +0300

    Add event listener for closing

--- src/components/Modals/Modal/Modal.jsx
+++ src/components/Modals/Modal/Modal.jsx
@@ -1,15 +1,38 @@
+import { useEffect, useState } from "react";
 import sprite from "../../../assets/icons/sprite.svg";
 
 import css from "./Modal.module.css";
 
 const Modal = () => {
+  const [isOpen, setIsOpen] = useState(true);
+
+  useEffect(() => {
+    const onEscPress = (evt) => {
+      if (evt.code === "Escape") {
+        setIsOpen(false);
+      }
+    };
+
+    document.addEventListener("keydown", onEscPress);
+
+    return () => {
+      document.removeEventListener("keydown", onEscPress);
+    };
+  }, [isOpen]);
+
+  const onCloseClick = (evt) => {
+    console.log("CLOSE PRESSED");
+    setIsOpen(!isOpen);
+  };
+
   return (
     <div className={css.container}>
-      <button className={css.closeBtn} type="button">
+      <button className={css.closeBtn} type="button" onClick={onCloseClick}>
         <svg className={css.closeSvg}>
           <use xlinkHref={`${sprite}#close`} />
         </svg>
       </button>
+      {isOpen && <p>OPEN</p>}
     </div>
   );
 };
