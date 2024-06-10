8106aa6ebe5b29dcb148737bbff57c5dbf6c0bab
Slava Poliakov <78slavapolyakov@gmail.com>
2024-06-11 00:01:04 +0300

    Add comments

--- src/components/layout/SharedLayout/SharedLayout.jsx
+++ src/components/layout/SharedLayout/SharedLayout.jsx
@@ -11,21 +11,32 @@ import LogOutModal from "../../Modals/LogOutModal/LogOutModal";
 import BurgerModal from "../../Modals/BurgerModal/BurgerModal";
 
 export const SharedLayout = () => {
+  /**
+   * delete this block after integration
+   * START
+   */
   const [isModal, setIsModal] = useState(false);
-
   const onModalClick = () => {
     setIsModal(!isModal);
   };
-
   const child1 = <SignUpModal title={"SIGN UP"} />;
   const child2 = <SignInModal title={"SIGN IN"} />;
   const child3 = <LogOutModal title={"LOG OUT"} />;
-
+  /**
+   * END
+   */
   return (
     <>
+      {/* delete it as well  - swap isModal to open different modals*/}
       <button type="button" onClick={onModalClick}>
         Open modal window - Temporary button - should be deleted
       </button>
+      {false && <Modal children={child1} onModalClick={onModalClick} />}
+      {isModal && <Modal children={child2} onModalClick={onModalClick} />}
+      {false && <Modal children={child3} onModalClick={onModalClick} />}
+      {false && <BurgerModal onBurgerMenuClick={onModalClick} />}
+      {/* delete it as well */}
+
       <Header />
       <main>
         <Suspense fallback={<div>Loading...</div>}>
@@ -34,10 +45,6 @@ export const SharedLayout = () => {
         </Suspense>
       </main>
       <Footer />
-      {false && <Modal children={child1} onModalClick={onModalClick} />}
-      {false && <Modal children={child2} onModalClick={onModalClick} />}
-      {false && <Modal children={child3} onModalClick={onModalClick} />}
-      {isModal && <BurgerModal onBurgerMenuClick={onModalClick} />}
     </>
   );
 };
