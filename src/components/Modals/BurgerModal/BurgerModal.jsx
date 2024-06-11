5b5051720d195e929b9b3791e7b23a558d4d538f
Slava Poliakov <78slavapolyakov@gmail.com>
2024-06-11 23:07:47 +0300

    Add Burger Menu Modal

--- src/components/Modals/BurgerModal/BurgerModal.jsx
+++ src/components/Modals/BurgerModal/BurgerModal.jsx
@@ -6,11 +6,11 @@ import sprite from "../../../assets/icons/sprite.svg";
 
 import styles from "./BurgerModal.module.css";
 
-export const BurgerModal = ({ onBurgerMenuClick }) => {
+export const BurgerModal = ({ open, onClose }) => {
   useEffect(() => {
     const onEscPress = (evt) => {
       if (evt.code === "Escape") {
-        onBurgerMenuClick();
+        onClose();
       }
     };
 
@@ -19,39 +19,43 @@ export const BurgerModal = ({ onBurgerMenuClick }) => {
     return () => {
       document.removeEventListener("keydown", onEscPress);
     };
-  }, [onBurgerMenuClick]);
+  }, [onClose]);
 
   return (
-    <div className={styles.backdrop}>
-      <div className={styles.header}>
-        <Logo />
-        <button
-          className={styles.closeBtn}
-          type="button"
-          onClick={() => onBurgerMenuClick()}
-        >
-          <svg className={styles.closeSvg}>
-            <use xlinkHref={`${sprite}#close`} />
-          </svg>
-        </button>
-      </div>
-      <ul className={styles.headerNavList}>
-        <li className={styles.headerNavItem}>
-          <NavLink to="/" className={styles.homeLink}>
-            Home
-          </NavLink>
-        </li>
-        <li className={styles.headerNavItem}>
-          <NavLink
-            to="/recipe"
-            className={styles.recipeLink}
-            state={{ recipe: "Add recipe" }}
-          >
-            Add recipe
-          </NavLink>
-        </li>
-      </ul>
-      <HeroImageContainer />
-    </div>
+    <>
+      {open && (
+        <div className={styles.backdrop}>
+          <div className={styles.header}>
+            <Logo />
+            <button
+              className={styles.closeBtn}
+              type="button"
+              onClick={() => onClose()}
+            >
+              <svg className={styles.closeSvg}>
+                <use xlinkHref={`${sprite}#close`} />
+              </svg>
+            </button>
+          </div>
+          <ul className={styles.headerNavList}>
+            <li className={styles.headerNavItem}>
+              <NavLink to="/" className={styles.homeLink}>
+                Home
+              </NavLink>
+            </li>
+            <li className={styles.headerNavItem}>
+              <NavLink
+                to="/recipe"
+                className={styles.recipeLink}
+                state={{ recipe: "Add recipe" }}
+              >
+                Add recipe
+              </NavLink>
+            </li>
+          </ul>
+          <HeroImageContainer />
+        </div>
+      )}
+    </>
   );
 };
--- src/components/layout/SharedLayout/SharedLayout.jsx
+++ src/components/layout/SharedLayout/SharedLayout.jsx
@@ -1,4 +1,4 @@
-import { Suspense, useState } from "react";
+import { Suspense } from "react";
 import { Outlet, useLocation } from "react-router-dom";
 
 import { Header } from "../Header";
@@ -6,27 +6,13 @@ import { Footer } from "../Footer";
 // TODO: will be fixed later
 // import { Breadcrumbs } from "../Breadcrumbs";
 import styles from "./SharedLayout.module.css";
-import { SignUpModal } from "../../Modals";
 
 export const SharedLayout = () => {
   const location = useLocation();
   const isHomePage = location.pathname === "/";
 
-  const [open, setOpen] = useState(false);
-
-  const onClickModal = (index) => {
-    setOpen(true);
-  };
-  const onClose = () => {
-    setOpen(false);
-  };
-
   return (
     <>
-      <button type="button" onClick={onClickModal}>
-        SignIn
-      </button>
-      <SignUpModal open={open} onClose={onClose} />
       <Header isHomePage={isHomePage} />
       <main className={!isHomePage && styles.paddingTop}>
         <Suspense fallback={<div>Loading...</div>}>
