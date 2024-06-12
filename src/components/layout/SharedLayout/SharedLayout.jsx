1c08f4887b55ec7459bf60e153ae3d799cde9197
Dmytro <tschernomord@gmail.com>
2024-06-12 19:44:27 +0200

    console-errrors

--- src/components/layout/SharedLayout/SharedLayout.jsx
+++ src/components/layout/SharedLayout/SharedLayout.jsx
@@ -1,6 +1,5 @@
 import classnames from "classnames";
 import { Suspense } from "react";
-
 import { Outlet, useLocation } from "react-router-dom";
 import { Header } from "../Header";
 import { Footer } from "../Footer";
