fb7ce1dc3b779ef74a5bdf33b4623b608de62aef
Dmytro <tschernomord@gmail.com>
2024-06-13 09:54:54 +0200

    cleanup

--- src/components/layout/Breadcrumbs/Breadcrumbs.js
+++ src/components/layout/Breadcrumbs/Breadcrumbs.js
@@ -1,4 +1,4 @@
-import React, { useMemo } from "react";
+import { useMemo } from "react";
 import classnames from "classnames";
 import { Link, useLocation } from "react-router-dom";
 import { useGetRecipeQuery } from "../../../redux";
