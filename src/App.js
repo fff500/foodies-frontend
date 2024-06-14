e5c22f7a7597f5017bb2814282ba0ec947ec4449
Dmytro <tschernomord@gmail.com>
2024-06-14 11:21:59 +0200

    add private routes

--- src/App.js
+++ src/App.js
@@ -1,21 +1,28 @@
 import { lazy } from "react";
 import { Routes, Route } from "react-router-dom";
 import { SharedLayout } from "./components";
+import { useIsAuth } from "./hooks/";
 
 const Home = lazy(() => import("./pages/Home/Home"));
 const User = lazy(() => import("./pages/User/User"));
 const AddRecipe = lazy(() => import("./pages/AddRecipe/AddRecipe"));
 const RecipePage = lazy(() => import("./pages/Recipe/RecipePage"));
+const PrivateRoutes = lazy(() => import("./pages/PrivateRoutes/PrivateRoutes"));
 
-const App = () => (
-  <Routes>
-    <Route path="/" element={<SharedLayout />}>
-      <Route index element={<Home />} />
-      <Route path="/user/:userId" element={<User />} />
-      <Route path="/recipe/:recipeId" element={<RecipePage />} />
-      <Route path="/recipe/add" element={<AddRecipe />} />
-      <Route path="*" element={<Home />} />
-    </Route>
-  </Routes>
-);
+const App = () => {
+  const { isAuth } = useIsAuth();
+  return (
+    <Routes>
+      <Route path="/" element={<SharedLayout />}>
+        <Route index element={<Home />} />
+        <Route path="/" element={<PrivateRoutes isAuth={isAuth} />}>
+          <Route path="user/:userId" element={<User />} />
+          <Route path="recipe/:recipeId" element={<RecipePage />} />
+          <Route path="recipe/add" element={<AddRecipe />} />
+        </Route>
+        <Route path="*" element={<Home />} />
+      </Route>
+    </Routes>
+  );
+};
 export default App;
