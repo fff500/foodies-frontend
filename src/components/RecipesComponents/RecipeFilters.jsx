e97fa506d22b623da3b54b2655ec248a083032f6
AnastasiaRiabova <riabova321@gmail.com>
2024-06-15 21:53:16 +0200

    change custom select to react select

--- src/components/RecipesComponents/RecipeFilters.jsx
+++ src/components/RecipesComponents/RecipeFilters.jsx
@@ -1,3 +1,4 @@
+import Select from "react-select";
 import { useSearchParams } from "react-router-dom";
 import { useGetAreasQuery, useGetIngredientsQuery } from "../../redux";
 import styles from "./Recipes.module.css";
@@ -13,75 +14,85 @@ export const RecipeFilters = () => {
   const { data: areasData = [], isLoading: areasIsLoading } =
     useGetAreasQuery();
 
-  const handleIngredientChange = (event) => {
-    const value = event.target.value;
-    const newSearchParams = new URLSearchParams(searchParams);
+  const newSearchParams = new URLSearchParams(searchParams);
+  const handleFilterChange = (select, field) => {
+    const value = field === "area" ? select.label : select.id;
     if (value) {
-      newSearchParams.set("ingredient", value);
+      newSearchParams.set(field, value);
     } else {
-      newSearchParams.delete("ingredient");
-    }
-    setSearchParams(newSearchParams);
-  };
-  const handleRegionChange = (event) => {
-    const value = event.target.value;
-    const newSearchParams = new URLSearchParams(searchParams);
-    if (value) {
-      newSearchParams.set("area", value);
-    } else {
-      newSearchParams.delete("area");
+      newSearchParams.delete(field);
     }
     setSearchParams(newSearchParams);
   };
 
+  const resetFilter = (field) => {
+    console.log(field);
+    newSearchParams.delete(field);
+  };
+
+  const mapedIngredients = ingredientsData.map((el) => ({
+    label: el.name,
+    id: el._id,
+    value: el._id,
+  }));
+
+  const mapedareasData = areasData.map((el) => ({
+    label: el.name,
+    id: el._id,
+    value: el._id,
+  }));
+
+  const customStyles = {
+    menu: (provided) => ({
+      // 'menu' is from the div class too.
+      ...provided,
+
+      zIndex: 100,
+    }),
+  };
   return (
     <div className={styles.filtersSelect}>
       <div className={styles.selectWrapper}>
         {!ingredientsIsLoading && (
           <>
-            <select
-              value={selectedIngredient}
-              onChange={handleIngredientChange}
-            >
-              <option value="">
-                {selectedIngredient ? "" : "Ingredients"}
-              </option>
-              {ingredientsData.map((ingredient) => (
-                <option key={ingredient._id} value={ingredient._id}>
-                  {ingredient.name}
-                </option>
-              ))}
-            </select>
-            <span className={styles.customArrow}>
-              <Icon
-                width={18}
-                height={18}
-                id={"dropDown"}
-                className={styles.dropDownIcon}
-              />
-            </span>
+            <Select
+              styles={customStyles}
+              className={styles.select}
+              placeholder="Ingredients"
+              onChange={(selected) =>
+                handleFilterChange(selected, "ingredients")
+              }
+              options={ingredientsData && mapedIngredients}
+            />
+            {selectedIngredient && (
+              <span
+                className={styles.customReset}
+                onClick={() => resetFilter("ingredients")}
+              >
+                <Icon width={18} height={18} id={"close"} />
+              </span>
+            )}
           </>
         )}
       </div>
       <div className={styles.selectWrapper}>
         {!areasIsLoading && (
           <>
-            <select value={selectedArea} onChange={handleRegionChange}>
-              <option value="">{selectedArea ? "" : "Area"}</option>
-              {areasData.map((region) => (
-                <option key={region._id} value={region.name}>
-                  {region.name}
-                </option>
-              ))}
-            </select>
-            <span className={styles.customArrow}>
-              <Icon
-                width={18}
-                height={18}
-                id={"dropDown"}
-                className={styles.dropDownIcon}
-              />
-            </span>
+            <Select
+              styles={customStyles}
+              className={styles.select}
+              placeholder="Area"
+              onChange={(selected) => handleFilterChange(selected, "area")}
+              options={areasData && mapedareasData}
+            />
+            {selectedArea && (
+              <span
+                className={styles.customReset}
+                onClick={() => resetFilter("area")}
+              >
+                <Icon width={18} height={18} id={"close"} />
+              </span>
+            )}
           </>
         )}
       </div>
