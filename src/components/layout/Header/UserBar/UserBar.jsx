89e293026cf5c98ebc6ad20812e0e57127075130
yevhenii-bezeha <bezegajenja0305@gmail.com>
2024-06-15 11:17:59 +0200

    User page minor fixes

--- src/components/layout/Header/UserBar/UserBar.jsx
+++ src/components/layout/Header/UserBar/UserBar.jsx
@@ -50,7 +50,7 @@ export const UserBar = ({ userName, userImage, id }) => {
             })}
           >
             <div>
-              <Link to={`/user/${id}`}>Profile</Link>
+              <Link to={"/user"}>Profile</Link>
             </div>
             <div>
               <Button
