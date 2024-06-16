41431e516352fe8462f9978088a1e92deef13d3f
Slava Poliakov <78slavapolyakov@gmail.com>
2024-06-16 14:57:30 +0300

    Add animation and logo redirection

--- src/components/Modals/BurgerModal/BurgerModal.jsx
+++ src/components/Modals/BurgerModal/BurgerModal.jsx
@@ -1,5 +1,6 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { NavLink } from "react-router-dom";
+import { useTransition, animated } from "react-spring";
 import { useFocusTrap } from "@mantine/hooks";
 import { useBodyScrollLock } from "../../../hooks";
 import { Button, Icon, Logo } from "../../shared";
@@ -26,43 +27,64 @@ export const BurgerModal = ({ isOpen, onClose }) => {
     onClose();
   };
 
+  const [isShow, setIsShow] = useState(true);
+  const transition = useTransition(isShow, {
+    from: { translateX: "100%" },
+    enter: { translateX: "0%" },
+    leave: { translateX: "100%" },
+  });
+  const onMenuClose = () => {
+    setIsShow(false);
+    setTimeout(() => onClose(), 400);
+  };
+
   return (
     <>
-      {isOpen && (
-        <div className={styles.backdrop} ref={focusTrapRef}>
-          <div className={styles.header}>
-            <Logo />
-            <Button
-              className={styles.closeBtn}
-              type="button"
-              onClick={() => onClose()}
-            >
-              <Icon className={styles.closeSvg} id={"close"} />
-            </Button>
-          </div>
-          <ul className={styles.headerNavList}>
-            <li className={styles.headerNavItem}>
-              <NavLink
-                to="/"
-                className={styles.homeLink}
-                onClick={() => handleLinkClick()}
-              >
-                Home
+      {transition((css, item) =>
+        item ? (
+          <animated.div
+            style={css}
+            className={styles.backdrop}
+            ref={focusTrapRef}
+          >
+            <div className={styles.header}>
+              <NavLink to="/" onClick={() => handleLinkClick()}>
+                <Logo />
               </NavLink>
-            </li>
-            <li className={styles.headerNavItem}>
-              <NavLink
-                to="/recipe/add"
-                className={styles.recipeLink}
-                state={{ recipe: "Add recipe" }}
-                onClick={() => handleLinkClick()}
+              <Button
+                className={styles.closeBtn}
+                type="button"
+                onClick={() => onMenuClose()}
               >
-                Add recipe
-              </NavLink>
-            </li>
-          </ul>
-          <HeroImageContainer />
-        </div>
+                <Icon className={styles.closeSvg} id={"close"} />
+              </Button>
+            </div>
+            <ul className={styles.headerNavList}>
+              <li className={styles.headerNavItem}>
+                <NavLink
+                  to="/"
+                  className={styles.homeLink}
+                  onClick={() => handleLinkClick()}
+                >
+                  Home
+                </NavLink>
+              </li>
+              <li className={styles.headerNavItem}>
+                <NavLink
+                  to="/recipe/add"
+                  className={styles.recipeLink}
+                  state={{ recipe: "Add recipe" }}
+                  onClick={() => handleLinkClick()}
+                >
+                  Add recipe
+                </NavLink>
+              </li>
+            </ul>
+            <HeroImageContainer />
+          </animated.div>
+        ) : (
+          ""
+        )
       )}
     </>
   );
